2dfefe571cbceaa517c0bd7a754cefbfc5e5149c
saenql <sae819702@gmail.com>
2026-02-25 18:40:06 -0500

    Implement continuous rendering with requestAnimationFrame
    Refactor rendering to use requestAnimationFrame for smoother animations.

--- main.js
+++ main.js
@@ -24,7 +24,20 @@ function main() {
 	const cube = new THREE.Mesh( geometry, material );
 	scene.add( cube );
 
-	renderer.render( scene, camera );
+	function render( time ) {
+
+		time *= 0.001; // convert time to seconds
+
+		cube.rotation.x = time;
+		cube.rotation.y = time;
+
+		renderer.render( scene, camera );
+
+		requestAnimationFrame( render );
+
+	}
+
+	requestAnimationFrame( render );
 
 }
 
